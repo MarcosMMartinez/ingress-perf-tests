1452619b6ad7017323570cf593d4398d3530cfcf
Andre Dewes <andredewes@microsoft.com>
2023-01-05 16:59:05 -0300

    Refactor and adding option to make parallel HTTP requests

--- AGICARMTesting/AGICARMTesting.cs
+++ AGICARMTesting/AGICARMTesting.cs
@@ -1,99 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
 
 
 
-using System.Diagnostics;
-using System.Net;
-using k8s;
-using k8s.Models;
+//using System.Diagnostics;
+//using System.Net;
+//using k8s;
+//using k8s.Models;
 
-string agicIp = "*.*.*.196";
-string nginxAffinityIp = "*.*.*.208";
-string nginxAntiAffinityIp = "*.*.*.138";
+//string agicIp = "*.*.*.196";
+//string nginxAffinityIp = "*.*.*.208";
+//string nginxAntiAffinityIp = "*.*.*.138";
 
-string affinityHost = "affinity";
-string antiaffinityHost = "antiaffinity";
+//string affinityHost = "affinity";
+//string antiaffinityHost = "antiaffinity";
 
-int totalRequests = 10;
-int remainingRequests = totalRequests;
-TimeSpan[] latencies = new TimeSpan[totalRequests];
-
-
-
-DeleteAndGo(nginxAffinityIp, affinityHost);
-
-void DeleteAndGo(string ip, string hostHeader)
-{
-    string appLabel = "";
-    string k8snamespace = "ingress-perf-testing";
-    int errorsThrown = 0;
-    int totalOk = 0;
+//int totalRequests = 10;
+//int remainingRequests = totalRequests;
+//TimeSpan[] latencies = new TimeSpan[totalRequests];
 
 
-    Random rnd = new Random();
+
+//DeleteAndGo(nginxAffinityIp, affinityHost);
+
+//void DeleteAndGo(string ip, string hostHeader)
+//{
+//    string appLabel = "";
+//    string k8snamespace = "ingress-perf-testing";
+//    int errorsThrown = 0;
+//    int totalOk = 0;
+
+
+//    Random rnd = new Random();
     
 
-    var kubeconfig = KubernetesClientConfiguration.BuildDefaultConfig();
-    var client = new Kubernetes(kubeconfig);
+//    var kubeconfig = KubernetesClientConfiguration.BuildDefaultConfig();
+//    var client = new Kubernetes(kubeconfig);
 
-    if (hostHeader == "affinity")
-    { appLabel = "app=affinity-dummies"; }
-    else { appLabel = "app=anti-affinity-dummies"; }
+//    if (hostHeader == "affinity")
+//    { appLabel = "app=affinity-dummies"; }
+//    else { appLabel = "app=anti-affinity-dummies"; }
 
 
 
-    for (int i = 1; i <= totalRequests; i++)
-    {
-        var myPods = client.ListNamespacedPodAsync(k8snamespace, labelSelector: appLabel);
-        int randomPodIndex = rnd.Next(0, myPods.Result.Items.Count);
-        client.DeleteNamespacedPodAsync(myPods.Result.Items[randomPodIndex].Name(), k8snamespace);
-        Console.WriteLine(DateTime.Now.ToString() + ": Deleting App Pod");
+//    for (int i = 1; i <= totalRequests; i++)
+//    {
+//        var myPods = client.ListNamespacedPodAsync(k8snamespace, labelSelector: appLabel);
+//        int randomPodIndex = rnd.Next(0, myPods.Result.Items.Count);
+//        client.DeleteNamespacedPodAsync(myPods.Result.Items[randomPodIndex].Name(), k8snamespace);
+//        Console.WriteLine(DateTime.Now.ToString() + ": Deleting App Pod");
 
-        Console.WriteLine(DateTime.Now.ToString() + ": Requests Remaining: " + remainingRequests);
+//        Console.WriteLine(DateTime.Now.ToString() + ": Requests Remaining: " + remainingRequests);
 
-            using (HttpClient httpClient = new HttpClient())
-            {                
-                Stopwatch stopwatch = new Stopwatch();
-                httpClient.DefaultRequestHeaders.Add("Host", hostHeader);
+//            using (HttpClient httpClient = new HttpClient())
+//            {                
+//                Stopwatch stopwatch = new Stopwatch();
+//                httpClient.DefaultRequestHeaders.Add("Host", hostHeader);
 
-                try
-                {
-                    stopwatch.Start();
-                    Console.WriteLine(DateTime.Now.ToString() + ": Request Kicked off.");
-                    HttpResponseMessage response = httpClient.GetAsync("http://" + ip).Result;
-                    stopwatch.Stop();
-                    Console.WriteLine("Request Latency (ms):" + stopwatch.Elapsed.TotalMilliseconds);
-                    latencies[i - 1] = stopwatch.Elapsed;
-                    if (response.StatusCode == HttpStatusCode.OK) { totalOk++; } else { errorsThrown++; Console.WriteLine("Error Thrown: " + response.StatusCode);  }
-                }
-                catch (Exception e)
-                {
-                    errorsThrown++;
-                    Console.WriteLine(e.Message);
-                }
+//                try
+//                {
+//                    stopwatch.Start();
+//                    Console.WriteLine(DateTime.Now.ToString() + ": Request Kicked off.");
+//                    HttpResponseMessage response = httpClient.GetAsync("http://" + ip).Result;
+//                    stopwatch.Stop();
+//                    Console.WriteLine("Request Latency (ms):" + stopwatch.Elapsed.TotalMilliseconds);
+//                    latencies[i - 1] = stopwatch.Elapsed;
+//                    if (response.StatusCode == HttpStatusCode.OK) { totalOk++; } else { errorsThrown++; Console.WriteLine("Error Thrown: " + response.StatusCode);  }
+//                }
+//                catch (Exception e)
+//                {
+//                    errorsThrown++;
+//                    Console.WriteLine(e.Message);
+//                }
                 
-                Thread.Sleep(500);
-            } 
+//                Thread.Sleep(500);
+//            } 
 
-        remainingRequests -= 1;
-    }
+//        remainingRequests -= 1;
+//    }
 
-    Console.WriteLine("Total Successes: " + totalOk);
-    Console.WriteLine("Total Errors: " + errorsThrown);
-    Console.WriteLine("Latency Average: " + averageLatency(latencies).TotalMilliseconds);
-}
+//    Console.WriteLine("Total Successes: " + totalOk);
+//    Console.WriteLine("Total Errors: " + errorsThrown);
+//    Console.WriteLine("Latency Average: " + averageLatency(latencies).TotalMilliseconds);
+//}
 
-TimeSpan averageLatency(TimeSpan[] latencies)
-{
-    TimeSpan ts = new TimeSpan();
+//TimeSpan averageLatency(TimeSpan[] latencies)
+//{
+//    TimeSpan ts = new TimeSpan();
 
-    for (int i = 0; i < latencies.Length; i++)
-    {
-        ts += latencies[i];
-    }
+//    for (int i = 0; i < latencies.Length; i++)
+//    {
+//        ts += latencies[i];
+//    }
 
-    return ts / latencies.Length;
-}
+//    return ts / latencies.Length;
+//}
 
 
 
